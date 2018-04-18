8d6eb235cc708704bc9c208aec94d2645f5d33a8
Zach Daniels <zach.daniels91@gmail.com>
2018-04-17 22:51:42 -0700

    Added Logging for activity cycles

--- ad340_mainapp/app/src/main/java/com/example/zach/ad340_mainapp/DisplayMessageActivity.java
+++ ad340_mainapp/app/src/main/java/com/example/zach/ad340_mainapp/DisplayMessageActivity.java
@@ -4,20 +4,66 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.util.Log;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+
+    private static final String TAG = DisplayMessageActivity.class.getSimpleName();
+    private String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra("message");
-
-        // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView4);
-        textView.setText(message);
+
+        if (savedInstanceState != null) {
+            Log.d(TAG, "onCreate Restoring previous state");
+            message = savedInstanceState.getString("message");
+            textView.setText(message);
+        } else {
+            Log.d(TAG, "onCreate No saved state available");
+            // Get the Intent that started this activity and extract the string
+            Intent intent = getIntent();
+            message = intent.getStringExtra("message");
+            textView.setText(message);
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart() called");
     }
 }
